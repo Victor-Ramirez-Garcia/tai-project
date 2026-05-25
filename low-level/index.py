eef61aebceb57de4739a80b0da2874653d597108
lowercasewords <87104601+lowercasewords@users.noreply.github.com>
2026-05-25 11:41:29 -0700

    deleting low-level folder

--- low-level/index.py
+++
@@ -1,9 +0,0 @@
-from leetscrape import GetQuestion
-
-# The 'titleSlug' is the last part of the URL, e.g., 'two-sum'
-problem = GetQuestion(titleSlug="two-sum").scrape()
-
-# 'problem' now contains a structured object with:
-# description, constraints, examples, code snippets, and more.
-import json
-print(json.dumps(problem, indent=4))
